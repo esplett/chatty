ea763616590e9b1c4a2d2e03579393744ddd4c32
Esther Splett <estherina@gmail.com>
2019-04-12 11:46:53 -0400

    tried to fix the chatbar at the bottom so that the boxes line up next to each other

--- src/Message.jsx
+++ src/Message.jsx
@@ -9,10 +9,6 @@ class Message extends Component {
     <span className="message-username">{this.props.message.username}</span>
     <span className="message-content">{this.props.message.content}</span>
     </div>
-    // <div className="message system">
-    // Georges Bataille changed their name to Cretin.
-    // </div>
-
     );
   }
 }
